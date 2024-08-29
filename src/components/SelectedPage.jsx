9af36730d1040c543341dea8bb5619d1a33da189
Ziyodullo <ziyodillo808@gmail.com>
2024-08-29 23:06:34 +0300

    umra page is fetching

--- src/components/SelectedPage.jsx
+++ src/components/SelectedPage.jsx
@@ -1,64 +1,138 @@
-import ExtraPagesHeader from './extraPagesHeader/ExtraPagesHeader';
-import ContactUs from './contactUs/ContactUs';
-import AnswerToQuestions from './answerToQuestions/AnswerToQuestions';
-import Loader from './../ui/Loader';
+import "./selectedPage.css";
+
+import ExtraPagesHeader from "./extraPagesHeader/ExtraPagesHeader";
+import ContactUs from "./contactUs/ContactUs";
+import AnswerToQuestions from "./answerToQuestions/AnswerToQuestions";
+import Loader from "./../ui/Loader";
 
 import kabah_1 from "../images/kabah_3.jpg";
+import Info from "../service/info";
+import useFetch from "../hooks/useFetch";
 
-function SelectedPage({ packages, loading, error }) {
-    const tourPackage = packages && packages[0] || {};
+function SelectedPage({ packages, loading, error, tours }) {
+  const tourPackage = (packages && packages[0]) || {};
+  const { data: info } = useFetch(Info.getInfo);
 
-    return (
-        <div className='tour-package'>
-            <ExtraPagesHeader title={tourPackage.name || "Paket tafsilotlari"} />
-            <div className="container">
-                <div className="tour-package-about">
-                    {loading ? <Loader/> : error ? <h2>{error.message}</h2> : <>
-                    <div className="tour-offer">
-                        <div className="main-image">
-                            <img src={tourPackage.image_path ? tourPackage.image_path : kabah_1} alt={tourPackage.name} />
-                        </div>
-                        <div className="title">
-                            <h1>{tourPackage.name}</h1>
-                        </div>
-                        <div className="description">
-                            <p>{tourPackage.description}</p>
-                        </div>
-                    </div>
-                    <div className="tour-package-items">
-                        <div className="title">
-                            <h1>{tourPackage.name || 'Paketlar'} paketlari</h1>
-                        </div>
-
-                        <div className="packages">
-                            {tourPackage.tour_packs && tourPackage.tour_packs.map((pack) => (
-                                <div className="package" key={pack.id}>
-                                    <div className="package-image">
-                                        <img src={'https://yusro.pythonanywhere.com' + pack.background_image_path} alt={pack.name} />
-                                    </div>
-                                    <div className="package-content">
-                                        <h3>{pack.name}</h3>
-                                        <ul>
-                                            {pack.pack_includes && pack.pack_includes.length > 0 ? pack.pack_includes.map((include) => (
-                                                <li key={include.id}><i className="fa-solid fa-check"></i>{include.text}</li>
-                                            )) : <li>Ma'lumot mavjud emas</li>}
-                                        </ul>
-                                        <span className="price">${pack.price}</span>
-                                        <button className="order-btn" onClick={() => handleOrderClick(pack)}>Batafsil</button>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                    </>}
+  return (
+    <div className="tour-package">
+      <ExtraPagesHeader title={tourPackage.name || "Paket tafsilotlari"} />
+      <div className="container first-container">
+        <div className="tour-package-about">
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <h2>{error.message}</h2>
+          ) : (
+            <>
+              <div className="tour-offer">
+                <div className="main-image">
+                  <img
+                    src={tourPackage.image ? tourPackage.image : kabah_1}
+                    alt={tourPackage.name}
+                  />
                 </div>
-                <div className="items">
-                    <ContactUs />
-                    <AnswerToQuestions />
+                <div className="title">
+                  <h1>{tourPackage.name}</h1>
                 </div>
-            </div>
+                <div className="description">
+                  <p>
+                    O'zbekistondagi eng tajribali tur agentligi, oxirgi{" "}
+                    {info.experience}+ yil davomida {info.travelers} dan ortiq
+                    Haj va Umra sayohatchilariga yordam bergan va Saudiya
+                    hukumatining ko'plab yutuqlari uchun mukofotlarni qo'lga
+                    kiritgan agentlik bilan birga ekaningizga 100% ishonch hosil
+                    qiling
+                  </p>
+                </div>
+                <div className="description">
+                  <p>
+                    Har bir inson Haj yoki Umra ziyoratini ado etish yo'lida
+                    to'siq bo'lib qolishi mumkin bo'lgan ba'zi muammolarga duch
+                    keladi. Aksariyat fuqarolar vizalar va pasportlarni
+                    rasmiylashtirish uchun to'g'ri vaqtda to'g'ri joyga murojaat
+                    qilmaydi va ko'p vaqt yo'qotadi. Siz ham vaqtingizni bekorga
+                    sarflamang!
+                  </p>
+                </div>
+                {/* <p className="offers-p">Sizga taklif etamiz:</p> */}
+                <ul className="offers-ul">
+                  <li>
+                    <i className="fa-solid fa-check"></i>
+                    Viza rasmiylashtirish
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-check"></i>
+                    50 boshi
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-check"></i>
+                    Avia chiptalar
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-check"></i>
+                    Shinam mehmonxona
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-check"></i>
+                    3 mahal ovqat
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-check"></i>
+                    Sovg'alar (nimcha, sumka, zam-zam suvi)
+                  </li>
+                </ul>
+              </div>
+            </>
+          )}
         </div>
-    );
+        <div className="items">
+          <ContactUs />
+          <AnswerToQuestions />
+        </div>
+      </div>
+      <div className="container">
+      <div className="tour-package-items">
+                <div className="title">
+                  <h1>{tourPackage.name || "Paketlar"} paketlari</h1>
+                </div>
+
+                <div className="packages">
+                  {tours &&
+                    tours.results?.map((pack) => (
+                      <div className="package" key={pack.id}>
+                        <div className="package-image">
+                          <img src={pack.image || ""} alt={pack.name || ""} />
+                        </div>
+                        <div className="package-content">
+                          <h3>{pack.name}</h3>
+                          <ul>
+                          <p>O'z ichiga oladi:</p>
+                            {pack.includes && pack.includes.length > 0 ? (
+                              pack.includes?.map((include) => (
+                                <li key={include.id}>
+                                  <i className="fa-solid fa-check"></i>
+                                  {include.name}
+                                </li>
+                              ))
+                            ) : (
+                              <li>Ma'lumot mavjud emas</li>
+                            )}
+                          </ul>
+                          <span className="price">${pack.price}</span>
+                          <button
+                            className="order-btn"
+                            onClick={() => handleOrderClick(pack)}
+                          >
+                            Batafsil
+                          </button>
+                        </div>
+                      </div>
+                    ))}
+                </div>
+              </div>
+      </div>
+    </div>
+  );
 }
 
 export default SelectedPage;
--- src/components/contactUs/ContactUs.jsx
+++ src/components/contactUs/ContactUs.jsx
@@ -1,103 +1,3 @@
-// import React, { useState } from 'react';
-// import './contactUs.css';
-// import axios from 'axios';
-// import UiInput from './../../ui/UiInput';
-// import UiTextarea from './../../ui/UiTextarea';
-// import ValidateForm from './../../helpers/ValidateForm';
-
-// function ContactUs() {
-//   const [name, setName] = useState('');
-//   const [phoneNumber, setPhoneNumber] = useState('');
-//   const [message, setMessage] = useState('');
-//   const [formErrors, setFormErrors] = useState({});
-//   const [placeholder, setPlaceholder] = useState({});
-//   const [successMessage, setSuccessMessage] = useState('');
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState(null);
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     const { errors, placeholders } = ValidateForm({ name, phoneNumber });
-// 		console.log(errors);
-
-//     if (Object.keys(errors).length > 0) {
-//       setFormErrors(errors);
-//       setPlaceholder(placeholders);
-//       setTimeout(() => setPlaceholder({}), 3000);
-//       return;
-//     }
-
-//     setIsLoading(true);
-//     const data = { full_name: name, phone_number: phoneNumber, text: message };
-
-//     try {
-//       const response = await axios.post('http://95.46.96.78:7777/api/v1/main/application-create/', data);
-//       if (response.status === 201) {
-//         setName('');
-//         setPhoneNumber('');
-//         setMessage('');
-//         setFormErrors({});
-//         setPlaceholder({});
-//         setSuccessMessage('Muvaffaqiyatli yuborildi');
-//         setTimeout(() => {
-//           setSuccessMessage('');
-//         }, 3000);
-//       } else {
-//         throw new Error('Xatolik yuz berdi!');
-//       }
-//     } catch (error) {
-//       const errorMessage = error?.response?.data?.errors || 'Bir xatolik yuz berdi!';
-//       setFormErrors({ global: errorMessage });
-//       setError(errorMessage);
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
-
-//   return (
-//     <div className='contact-us-section'>
-//       <div className='form-wrapper'>
-//         {successMessage ? (
-//           <div className='success-message'>
-//             <h2>{successMessage}</h2>
-//           </div>
-//         ) : (
-//           <>
-//             <h2 className='title'>Hoziroq ro'yxatdan o'ting</h2>
-//             <p>va chegirmaga ega bo'ling</p>
-//             <form onSubmit={handleSubmit}>
-//               <UiInput
-//                 type='text'
-//                 placeholder={placeholder.name || 'Ismingizni kiriting'}
-//                 state={name}
-//                 setState={setName}
-//                 hasError={!!formErrors.name}
-//               />
-//               <UiInput
-//                 type='text'
-//                 placeholder={placeholder.phoneNumber || 'Telefon raqamingizni kiriting'}
-//                 state={phoneNumber}
-//                 setState={setPhoneNumber}
-//                 hasError={!!formErrors.phoneNumber}
-//               />
-//               <UiTextarea
-//                 placeholder='Xabar'
-//                 state={message}
-//                 setState={setMessage}
-//               />
-//               <button type='submit' disabled={isLoading}>{isLoading ? 'YUBORILMOQDA...' : 'YUBORISH'}</button>
-//               {formErrors.global && <p className="error">{formErrors.global}</p>}
-//               {error && <p className="error">{error}</p>}
-//             </form>
-//           </>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ContactUs;
-
 import React, { useState } from "react";
 import axios from "axios";
 import sec_pattern from "../../icons/sec_pattern.png";
@@ -221,10 +121,16 @@ function ContactUs() {
                   />
                 </div>
                 <button type="submit" disabled={isLoading}>
-                  {isLoading ? "YUBORILMOQDA..." : "YUBORISH"}
+                  {isLoading ? (
+                    <i class="fa-solid fa-spinner"></i>
+                  ) : formErrors.global ? (
+                    formErrors.global
+                  ) : successMessage ? (
+                    <i class="fa-solid fa-check"></i>
+                  ) : (
+                    "Yuborish"
+                  )}
                 </button>
-                {/* {formErrors.global && <p className="error">{formErrors.global}</p>}
-              {error && <p className="error">{error}</p>} */}
               </form>
             </>
           )}
