a9cbfbef4430531e71b7c35cdcd48a505c64ba26
Ziyodullo <ziyodillo808@gmail.com>
2024-09-02 20:13:28 +0300

    some pages edited

--- src/pages/haj/Haj.jsx
+++ src/pages/haj/Haj.jsx
@@ -1,18 +1,86 @@
-import useFetch from '../../hooks/useFetch';
-import Info from '../../service/info';
+// import useFetch from '../../hooks/useFetch';
+// import Info from '../../service/info';
+// import SelectedPage from './../../components/SelectedPage';
+// import Travel from './../../service/travel';
+
+
+
+// function Haj() {
+//     const { data: info } = useFetch(Info.getInfo)    
+//     const { data, loading, error } = useFetch(Travel.getPlaces);
+//     const relevantPlaces = data.results?.filter(place => place.id === info.haj);
+
+//     return (
+//         <div className='haj-page'>
+//             <SelectedPage packages={relevantPlaces} error={error} loading={loading} />
+//         </div>
+//     );
+// }
+
+// export default Haj;
+
+
+// import useFetch from '../../hooks/useFetch';
+// import SelectedPage from './../../components/SelectedPage';
+// import Travel from './../../service/travel';
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
+
+// function Haj() {
+//     const { data: info } = useFetch(Travel.getPlaces);
+//     const [tours, setTours] = useState([]);
+//     const main_url = "http://95.46.96.78:7777/api/v1";
+
+//     useEffect(() => {
+//         if (info?.haj) {
+//             axios
+//                 .get(`${main_url}/tours/by-category/${info.haj}/`)
+//                 .then((response) => setTours(response.data.results || []))
+//                 .catch(() => {
+//                     throw new Error("Xatolik yuz berdi!");
+//                 });
+//         }
+//     }, [info?.haj]);
+
+//     return (
+//         <div className='haj-page'>
+//             <SelectedPage packages={info?.results || []} tours={tours} />
+//         </div>
+//     );
+// }
+
+// export default Haj;
+
+
 import SelectedPage from './../../components/SelectedPage';
-import Travel from './../../service/travel';
-
-
+import useFetch from '../../hooks/useFetch';
+import Travel from '../../service/travel';
+import Info from '../../service/info';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Haj() {
-    const { data: info } = useFetch(Info.getInfo)    
-    const { data, loading, error } = useFetch(Travel.getPlaces);
-    const relevantPlaces = data.results?.filter(place => place.id === info.haj);
+    const { data: info } = useFetch(Info.getInfo);  // Fetch Haj-specific info
+    const [tours, setTours] = useState([]);
+    const main_url = "http://95.46.96.78:7777/api/v1";
+
+    useEffect(() => {
+        if (info?.haj) {
+            axios
+                .get(`${main_url}/tours/by-category/${info.haj}/`)
+                .then((response) => setTours(response.data.results || []))
+                .catch(() => {
+                    throw new Error("Xatolik yuz berdi!");
+                });
+        }
+    }, [info?.haj]);
 
     return (
         <div className='haj-page'>
-            <SelectedPage packages={relevantPlaces} error={error} loading={loading} />
+            <SelectedPage 
+                packages={info?.results?.filter(place => place.id === info.haj) || []} 
+                tours={tours} 
+            />
         </div>
     );
 }
--- src/service/blog.js
+++ src/service/blog.js
@@ -35,14 +35,12 @@ const BlogArticle = {
 
   // post comment
   async postComment(id) {
-    // console.log(id);
     const response = await axios.post(`/main/comments/by-post/${id}/`);
     return response.data;
   },
 
   // post comment
   async fetchComments(id) {
-    // console.log(id);
     const response = await axios.get(`/main/comments/by-post/${id}/`);
     return response.data;
   },
@@ -50,46 +48,3 @@ const BlogArticle = {
 };
 
 export default BlogArticle;
-
-
-// import axios from './api';
-
-// const BlogArticle = {
-//   async fetchArticles(params) {
-//     try {
-//       const response = await axios.get('/post/articles/', { params });
-//       return response.data;
-//     } catch (error) {
-//       throw error.response || new Error('Unknown error');
-//     }
-//   },
-
-//   async fetchArticleSection() {
-//     const response = await axios.get('/post/categories/');
-//     return response.data;
-//   },
-//   async fetchArticlePopular(params) {
-//     const response = await axios.get('/post/tags/', { params });
-//     return response.data;
-//   },
-
-//   async fetchArticleTag(params) {
-//     const response = await axios.get('/post/tags/', { params });
-//     return response.data;
-//   },
-
-//   async readArticle(id) {
-//     const response = await axios.get(`/post/articles/${id}`);
-//     return response.data;
-//   },
-
-//   // post comment
-//   async postComment(user) {
-//     console.log(user);
-//     const response = await axios.post(`/post/comments/`, user);
-//     return response.data;
-//   },
-  
-// };
-
-// export default BlogArticle;
