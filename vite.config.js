d541710f46005a5ba7e17fa3c2fb19c1ced1054f
Ziyodullo <ziyodillo808@gmail.com>
2024-09-07 10:19:46 +0300

    vite config edited

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://95.46.96.78:7777', // Backend serveringiz URL manzili
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api/v1'), // Yo'lda o'zgarishlar kiritish
-      },
-    },
-  },
 })
